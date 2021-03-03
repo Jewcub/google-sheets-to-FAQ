386cf0d2ba73447616ce097237c426380fdedf94
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-03-03 18:16:04 +0800

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import Body from "./components/Body.jsx";
+import { ImSpinner3 } from "react-icons/im";
 function App() {
   const [faqJSON, setfaqJSON] = useState({ sections: null });
   const start = async () => {
@@ -14,9 +15,17 @@ function App() {
     start();
   }, []);
   return (
-    <div className="App">
-      <h1> KAVA FAQ</h1>
-      {faqJSON.sections ? <Body content={faqJSON}></Body> : "loading...."}
+    <div className="App scroller">
+      <div className="main-wrapper">
+        <h1 className="main-title"> KAVA FAQ</h1>
+        {faqJSON.sections ? (
+          <Body content={faqJSON}></Body>
+        ) : (
+          <div className="spinner-wrapper">
+            <ImSpinner3 className="loading-spinner" />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -6,15 +6,20 @@ export default function Body({ content }) {
   console.log({ content });
   return (
     <div className="faq-body">
-      {content.sections.map((section, i) => (
-        <button
-          className="section-title"
-          key={i}
-          onClick={() => setSelected(section.name)}
-        >
-          {section.name}
-        </button>
-      ))}
+      <div className="button-bar">
+        {content.sections.map((section, i) => (
+          <div
+            className={`section-title ${
+              selected === section.name ? "section-title-selected" : ""
+            }`}
+            key={i}
+            onClick={() => setSelected(section.name)}
+          >
+            <h4> {section.name}</h4>
+          </div>
+        ))}
+      </div>
+
       {content.sections.map((section, i) =>
         section.name === selected ? (
           <Section className="section-body" key={i} section={section}></Section>
